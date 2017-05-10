2314d064850d352e56a6444ce9abcada6635267d
sivakumart <sivakumartskl5a2@gmail.com>
2017-05-11 02:08:27 +0530

    Added ng-model for searchbox in navbar

--- app/controllers/navbarController.js
+++ app/controllers/navbarController.js
@@ -1,7 +1,4 @@
 navbar.controller('navbarController', function ($scope) {
     var vm = this;
-    vm.showPersonsObjects = function (event) {
-        event.preventDefault();
-        alert("Hi, you pressed the key...");
-    };
+    
 });
